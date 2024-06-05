08959d2e83d45b64fdbc6efc7531583e10ac0e2c
sim2g <mrqdodman@gmail.com>
2024-06-05 17:48:17 +0100

    Test .gitignore

---
+++ anfis-jax.py
@@ -0,0 +1,24 @@
+import jax.numpy as jnp
+
+
+class ANFIS:
+
+    def __init__(self, X, y, membershipFn, epochs):
+        pass
+
+
+    def LSE(self):
+        pass
+
+    def training(self):
+        pass
+
+    def forward_pass(self):
+        pass
+
+
+    def backprop(self):
+        pass
+
+    def pred(self):
+        pass
