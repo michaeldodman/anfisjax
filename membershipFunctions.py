e563ff152d1bfb9d5f797c94fa88532ab59e22b2
sim2g <mrqdodman@gmail.com>
2024-07-01 01:03:39 +0100

    Refactoring MembershipFunctions class

--- membershipFunctions.py
+++ membershipFunctions.py
@@ -3,126 +3,91 @@ import skfuzzy as fuzz
 import matplotlib.pyplot as plt
 
 
-class membershipFunctions:
+class MembershipFunctions:
     def __init__(self, X, **kwargs):
+        self.X = self._validate_input(X)
+        self.input_columns = self.X.shape[0]
+        self.sigma = jnp.std(self.X, axis=1)
+        self.lower_bound = jnp.min(self.X, axis=1)
+        self.upper_bound = jnp.max(self.X, axis=1)
+
+        mf_specs = self._parse_kwargs(kwargs)
+        self.membership_functions = self._initialize_membership_functions(
+            mf_specs, kwargs
+        )
+
+    def __str__(self):
+        output = "Membership Functions:\n"
+        for name, mf in self.membership_functions.items():
+            output += f"  {name}:\n"
+            output += f"    Type: {mf['type']}\n"
+            output += f"    Lower Bound: {mf['lower_bound']}\n"
+            output += f"    Upper Bound: {mf['upper_bound']}\n"
+            output += "    Parameters:\n"
+            for i, params in enumerate(mf['params']):
+                output += f"      MF {i+1}: {params}\n"
+            output += "\n"
+        return output
+
+    def _validate_input(self, X):
         if isinstance(X, list):
-            X = jnp.array(X)
+            return jnp.array(X)
         elif isinstance(X, jnp.ndarray):
-            pass
+            return X
         else:
             raise ValueError("Invalid input type for X. Expected list or jnp.ndarray.")
 
-        input_columns = X.shape[0]
-        sigma = jnp.std(X, axis=1)
-
-        self.membership_functions = dict()
-        self.lower_bound = jnp.min(X, axis=1)
-        self.upper_bound = jnp.max(X, axis=1)
-        mf_dict = {
-            "gaussian": gaussian,
-            "gbell": gbell,
-            "trapezoidal": trapezoidal,
-            "triangular": triangular,
-            "sigmoid": sigmoid,
-        }
-
+    def _parse_kwargs(self, kwargs):
         if "type" in kwargs and "num" in kwargs:
             mf_type = kwargs["type"]
             mf_num = kwargs["num"]
-            mf_specs = [(mf_type, mf_num)] * input_columns
+            return [(mf_type, mf_num)] * self.input_columns
         elif "mf_specs" in kwargs:
             mf_specs = kwargs["mf_specs"]
             if not isinstance(mf_specs, list) or not all(
                 isinstance(spec, tuple) and len(spec) == 2 for spec in mf_specs
             ):
                 raise ValueError("Invalid membership function specification")
-            if len(mf_specs) != input_columns:
+            if len(mf_specs) != self.input_columns:
                 raise ValueError(
                     "Number of membership function specifications must match the number of inputs"
                 )
+            return mf_specs
         else:
             raise ValueError(
                 "Invalid input format. Please provide either 'type' and 'num' or 'mf_specs'."
             )
 
-        for i in range(input_columns):
-            mf_type, mf_num = mf_specs[i]
-
-            if mf_type in mf_dict:
-                mf_class = mf_dict[mf_type]
-                if mf_class is gaussian:
-                    mf = mf_class(
-                        self.lower_bound[i], self.upper_bound[i], mf_num, sigma[i]
+    def _initialize_membership_functions(self, mf_specs, kwargs):
+        membership_functions = {}
+        for i, (mf_type, mf_num) in enumerate(mf_specs):
+            if mf_type in MEMBERSHIP_FUNCTION_DICT:
+                mf_func = MEMBERSHIP_FUNCTION_DICT[mf_type]
+                if mf_func is MembershipFunctions.gaussian:
+                    params = mf_func(
+                        self.lower_bound[i], self.upper_bound[i], mf_num, self.sigma[i]
                     )
                 else:
-                    mf = mf_class(self.lower_bound[i], self.upper_bound[i], mf_num)
+                    params = mf_func(self.lower_bound[i], self.upper_bound[i], mf_num)
+                
+                if "names" in kwargs:
+                    name = kwargs["names"][i]
+                else:
+                    name = f"{mf_type}x{mf_num}_{i+1}"
+                
+                membership_functions[name] = {
+                    "type": mf_type,
+                    "params": params,
+                    "lower_bound": self.lower_bound[i],
+                    "upper_bound": self.upper_bound[i]
+                }
             else:
                 raise ValueError(f"Invalid membership function type: {mf_type}")
-            if "names" in kwargs:
-                self.membership_functions[kwargs["names"][i]] = mf
-            else:
-                self.membership_functions[f"{mf_type}x{mf_num}_{i+1}"] = mf
 
-    def plot(self):
-        mf_dict = {
-            "gaussian": fuzz.gaussmf,
-            "gbell": fuzz.gbellmf,
-            "trapezoidal": fuzz.trapmf,
-            "triangular": fuzz.trimf,
-            "sigmoid": fuzz.sigmf,
-        }
-
-        num_subplots = len(self.membership_functions)
-        num_cols = int(num_subplots**0.5)
-        num_rows = (num_subplots + num_cols - 1) // num_cols
-
-        fig, axes = plt.subplots(nrows=num_rows, ncols=num_cols, figsize=(8, 8))
-        axes = axes.flatten()
-
-        linewidth = 2.0  # Set the desired line width
-        ylim_margin = linewidth / (2 * 72)  # Calculate the margin based on line width
-
-        for subplot_index, (name, obj) in enumerate(self.membership_functions.items()):
-            x = jnp.linspace(
-                start=obj.lower_bound,
-                stop=obj.upper_bound,
-                num=round(obj.upper_bound - obj.lower_bound) * 100,
-            )
-            class_name = str(obj.__class__).split(".")[-1]
-            mf_type = class_name.split("'")[0]
-
-            for _, params in enumerate(obj.parameters):
-                skfuzzy_func = mf_dict[mf_type]
-                if mf_type in ["gaussian", "gbell", "sigmoid"]:
-                    mem_func = skfuzzy_func(x, *params)
-                else:
-                    mem_func = skfuzzy_func(x, params)
-
-                axes[subplot_index].plot(x, mem_func)
-
-            axes[subplot_index].set_title(name)
-            axes[subplot_index].set_xlabel("x")
-            axes[subplot_index].set_ylabel("Membership")
-            axes[subplot_index].set_xlim([obj.lower_bound, obj.upper_bound])
-            axes[subplot_index].set_ylim([0, 1 + ylim_margin])
-
-        for i in range(num_subplots, len(axes)):
-            fig.delaxes(axes[i])
-
-        plt.tight_layout()
-        plt.show()
-
-
-class gaussian(membershipFunctions):
-    def __init__(self, lower_bound, upper_bound, n, sigma):
-        self.lower_bound = lower_bound
-        self.upper_bound = upper_bound
-        self.parameters = gaussian.initialize_parameters(
-            lower_bound, upper_bound, n, sigma
-        )
+        return membership_functions
 
     @staticmethod
-    def initialize_parameters(lower_bound, upper_bound, n, sigma):
+    def gaussian(lower_bound, upper_bound, n, sigma):
         if n == 1:
             return jnp.array([[lower_bound + ((upper_bound - lower_bound) / 2), sigma]])
         elif n == 2:
@@ -135,15 +100,8 @@ class gaussian(membershipFunctions):
             sigma_arr = jnp.ones(n) * (sigma / (n - 1))
             return jnp.stack([m, sigma_arr], axis=1)
 
-
-class gbell(membershipFunctions):
-    def __init__(self, lower_bound, upper_bound, n):
-        self.lower_bound = lower_bound
-        self.upper_bound = upper_bound
-        self.parameters = gbell.initialize_parameters(lower_bound, upper_bound, n)
-
     @staticmethod
-    def initialize_parameters(lower_bound, upper_bound, n):
+    def gbell(lower_bound, upper_bound, n):
         domain = upper_bound - lower_bound
         if n == 1:
             return jnp.array([domain / 4, 1, lower_bound + domain / 2])
@@ -162,15 +120,8 @@ class gbell(membershipFunctions):
             parameters = jnp.stack([widths, heights, centers], axis=1)
             return parameters
 
-
-class trapezoidal(membershipFunctions):
-    def __init__(self, lower_bound, upper_bound, n):
-        self.lower_bound = lower_bound
-        self.upper_bound = upper_bound
-        self.parameters = trapezoidal.initialize_parameters(lower_bound, upper_bound, n)
-
     @staticmethod
-    def initialize_parameters(lower_bound, upper_bound, n):
+    def trapezoidal(lower_bound, upper_bound, n):
         if n == 1:
             domain = upper_bound - lower_bound
             return jnp.array(
@@ -214,15 +165,8 @@ class trapezoidal(membershipFunctions):
             )
             return parameters
 
-
-class triangular(membershipFunctions):
-    def __init__(self, lower_bound, upper_bound, n):
-        self.lower_bound = lower_bound
-        self.upper_bound = upper_bound
-        self.parameters = triangular.initialize_parameters(lower_bound, upper_bound, n)
-
     @staticmethod
-    def initialize_parameters(lower_bound, upper_bound, n):
+    def triangular(lower_bound, upper_bound, n):
         if n == 1:
             return jnp.array(
                 [[lower_bound, (lower_bound + upper_bound) / 2, upper_bound]]
@@ -244,15 +188,8 @@ class triangular(membershipFunctions):
             parameters = jnp.stack([left_points, centers, right_points], axis=1)
             return parameters
 
-
-class sigmoid(membershipFunctions):
-    def __init__(self, lower_bound, upper_bound, n):
-        self.lower_bound = lower_bound
-        self.upper_bound = upper_bound
-        self.parameters = sigmoid.initialize_parameters(lower_bound, upper_bound, n)
-
     @staticmethod
-    def initialize_parameters(lower_bound, upper_bound, n):
+    def sigmoid(lower_bound, upper_bound, n):
         if n == 1:
             return jnp.array([[(lower_bound + upper_bound) / 2, 1]])
         elif n == 2:
@@ -266,6 +203,63 @@ class sigmoid(membershipFunctions):
             b_arr = jnp.linspace(start=lower_bound, stop=upper_bound, num=n)
             return jnp.stack([b_arr, jnp.ones(n) * (0.5 * n + 0.5)], axis=1)
 
+    def plot(self):
+        mf_dict = {
+            "gaussian": fuzz.gaussmf,
+            "gbell": fuzz.gbellmf,
+            "trapezoidal": fuzz.trapmf,
+            "triangular": fuzz.trimf,
+            "sigmoid": fuzz.sigmf,
+        }
+
+        num_subplots = len(self.membership_functions)
+        num_cols = int(num_subplots**0.5)
+        num_rows = (num_subplots + num_cols - 1) // num_cols
+
+        fig, axes = plt.subplots(nrows=num_rows, ncols=num_cols, figsize=(8, 8))
+        axes = axes.flatten()
+
+        linewidth = 2.0
+        ylim_margin = linewidth / (2 * 72)
+
+        for subplot_index, (name, mf) in enumerate(self.membership_functions.items()):
+            x = jnp.linspace(
+                start=mf['lower_bound'],
+                stop=mf['upper_bound'],
+                num=round(mf['upper_bound'] - mf['lower_bound']) * 100,
+            )
+            mf_type = mf['type']
+
+            skfuzzy_func = mf_dict[mf_type]
+            if mf_type in ["gaussian", "gbell", "sigmoid"]:
+                for params in mf['params']:
+                    mem_func = skfuzzy_func(x, *params)
+                    axes[subplot_index].plot(x, mem_func)
+            else:
+                for params in mf['params']:
+                    mem_func = skfuzzy_func(x, params)
+                    axes[subplot_index].plot(x, mem_func)
+
+            axes[subplot_index].set_title(name)
+            axes[subplot_index].set_xlabel("x")
+            axes[subplot_index].set_ylabel("Membership")
+            axes[subplot_index].set_xlim([mf['lower_bound'], mf['upper_bound']])
+            axes[subplot_index].set_ylim([0, 1 + ylim_margin])
+
+        for i in range(num_subplots, len(axes)):
+            fig.delaxes(axes[i])
+
+        plt.tight_layout()
+        plt.show()
+
+
+MEMBERSHIP_FUNCTION_DICT = {
+    "gaussian": MembershipFunctions.gaussian,
+    "gbell": MembershipFunctions.gbell,
+    "trapezoidal": MembershipFunctions.trapezoidal,
+    "triangular": MembershipFunctions.triangular,
+    "sigmoid": MembershipFunctions.sigmoid,
+}
 
 if __name__ == "__main__":
     pass
