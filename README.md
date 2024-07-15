59668cf3047eb7ff751d405aaebff401a8b5c457
sim2g <mrqdodman@gmail.com>
2024-07-15 16:03:39 +0100

    initial commit for Equinox refactor

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 A library for training ANFIS models using Jax.
 
 In progress.
+
+Currently refactoring to use the Equinox module.
--- anfis_jax_equinox.py
+++ anfis_jax_equinox.py
@@ -3,3 +3,7 @@ import jax
 import skfuzzy as fuzz
 import matplotlib.pyplot as plt
 import equinox as eqx
+
+class ANFIS(eqx.Module):
+    def __init__(self) -> None:
+        pass
