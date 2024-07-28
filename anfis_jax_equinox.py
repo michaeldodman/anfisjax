0c22749dddfe24297d5f3f0d734656795311fa34
sim2g <mrqdodman@gmail.com>
2024-07-29 00:18:08 +0100

    equinox structure

--- anfis_jax_equinox.py
+++ anfis_jax_equinox.py
@@ -4,6 +4,36 @@ import skfuzzy as fuzz
 import matplotlib.pyplot as plt
 import equinox as eqx
 
+class FuzzificationLayer(eqx.Module):
+    membership_functions: list # this will be a list of membership function instances
+    
+    def __init__(self, membership_functions):
+        pass
+
+    def __call__(self, x):
+        pass
+
+class RuleLayer(eqx.Module):
+    def __call__(self, fuzzified_input):
+        pass
+
+class NormalizationLayer(eqx.Module):
+    def __call__(self, firing_strengths):
+        pass
+
+class ConsequentLayer(eqx.Module):
+    coefficients: jnp.ndarray
+
+    def __init__(self, num_rules, num_features):
+        pass
+
+    def __call__(self):
+        pass
+
+class DefuzzificationLayer(eqx.Module):
+    def __call__self():
+        pass
+
 class ANFIS(eqx.Module):
     def __init__(self) -> None:
         pass
